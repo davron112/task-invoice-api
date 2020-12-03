7cca4b15e3fb924ef33099e1dced1a780f8e42d6
Davronbek Achilov <davron211@gmail.com>
2020-12-03 11:42:03 +0500

    Fixes interface

--- app/Repositories/SchoolRepositoryEloquent.php
+++ app/Repositories/SchoolRepositoryEloquent.php
@@ -2,9 +2,8 @@
 
 namespace App\Repositories;
 
-use App\Models\Invoice;
 use App\Models\School;
-use App\Repositories\Abstracts\InvoiceRepository;
+use App\Repositories\Abstracts\SchoolRepository;
 use Prettus\Repository\Eloquent\BaseRepository;
 use Prettus\Repository\Criteria\RequestCriteria;
 
@@ -12,7 +11,7 @@ use Prettus\Repository\Criteria\RequestCriteria;
  * Class SchoolRepositoryEloquent
  * @package App\Repositories
  */
-class SchoolRepositoryEloquent extends BaseRepository implements InvoiceRepository
+class SchoolRepositoryEloquent extends BaseRepository implements SchoolRepository
 {
     /**
      * Specify Model class name
