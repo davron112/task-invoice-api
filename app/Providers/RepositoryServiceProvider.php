86d1dfedcac3738ceec96bbf5ea7b5f36f60cb63
Davronbek Achilov <davron211@gmail.com>
2020-12-03 11:29:03 +0500

    Fixes

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use ;
+use App\Services\Contracts\PaymentService as PaymentServiceInterface;
+use App\Services\Contracts\InvoiceService as InvoiceServiceInterface;
 use Illuminate\Support\ServiceProvider;
 
 class RepositoryServiceProvider extends ServiceProvider
@@ -27,6 +30,10 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind(\App\Repositories\Abstracts\InvoiceRepository::class, \App\Repositories\InvoiceRepositoryEloquent::class);
         $this->app->bind(\App\Repositories\Abstracts\SchoolRepository::class, \App\Repositories\SchoolRepositoryEloquent::class);
         $this->app->bind(\App\Repositories\Abstracts\UserRepository::class, \App\Repositories\UserRepositoryEloquent::class);
+
+
+        $this->app->bind(PaymentServiceInterface::class, \App\Services\PaymentService::class);
+        $this->app->bind(InvoiceServiceInterface::class, \App\Services\InvoiceService::class);
         //:end-bindings:
     }
 }
